d21d8dfb498dad7b9f4846a7c4cb5b5a9c8e4510
rudybodo <transkumba@gmail.com>
2016-03-10 17:42:41 +0800

    maintenence detail function

--- app/controllers/HotelController.php
+++ app/controllers/HotelController.php
@@ -12,11 +12,19 @@ class HotelController extends ControllerBase
     public function detailAction($hotelsId)
     {
         $hotel = Hotels::findFirst($hotelsId);
+        $facility = Hotelsfacility::findFirstByHotelid($hotelsId);
         $city = $hotel->city;
         $province = $hotel->province;
         $country = $hotel->country;
 
         $this->view->detail = $hotel;
+        $this->view->facility = $facility;
+    }
+
+    public function addAction()
+    {
+        $hotel = new Hotels;
+
     }
 
 }
