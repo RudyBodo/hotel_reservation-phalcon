175fba54de3829389fb0d7fe14698ad033600fb1
rudybodo <transkumba@gmail.com>
2016-03-11 00:47:23 +0800

    add static file

--- app/cache/_home_rudybodo_hotel_reservation-phalcon_app_views_hotel_detail.volt.php
+++ app/cache/_home_rudybodo_hotel_reservation-phalcon_app_views_hotel_detail.volt.php
@@ -1,2 +1,40 @@
 <h1>Detail Hotel</h1>
 
+<table class="table">
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Address</th>
+        <th>City</th>
+        <th>Province</th>
+        <th>Country</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <td><?php echo $detail->name; ?></td>
+    <td><?php echo $detail->address; ?></td>
+    <td><?php echo $detail->city->city; ?></td>
+    <td><?php echo $detail->province->province; ?></td>
+    <td><?php echo $detail->country->country; ?></td>
+     </tbody>
+
+</table>
+<br>
+<h1>Facility</h1>
+
+<table class="table">
+    <thead>
+    <tr>
+        <td>Garage</td>
+        <td>Carports</td>
+        <td>Swimming Pool</td>
+    </tr>
+    </thead>
+
+    <tbody>
+    <td><?php echo $facility->garage; ?></td>
+    <td><?php echo $facility->carports; ?></td>
+    <td><?php echo $facility->swimmingpool; ?></td>
+    </tbody>
+</table>
