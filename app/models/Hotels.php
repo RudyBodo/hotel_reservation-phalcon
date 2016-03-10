aa654f2e3a67d9ea4e6a9021f7bbdf1c9f038a14
rudybodo <transkumba@gmail.com>
2016-03-10 16:41:00 +0800

    add relation table on city,province,country

---
+++ app/models/Hotels.php
@@ -0,0 +1,94 @@
+<?php
+
+class Hotels extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     */
+    public $address;
+
+    /**
+     *
+     * @var integer
+     */
+    public $zipcode;
+
+    /**
+     *
+     * @var double
+     */
+    public $price;
+
+    /**
+     *
+     * @var integer
+     */
+    public $city_id;
+
+    /**
+     *
+     * @var integer
+     */
+    public $province_id;
+
+    /**
+     *
+     * @var integer
+     */
+    public $country_id;
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function initialize()
+    {
+        $this->belongsTo("city_id", "City", "id");
+        $this->belongsTo("province_id", "Province", "id");
+        $this->belongsTo("country_id", "Country", "id");
+
+    }
+
+    public function getSource()
+    {
+        return 'hotels';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Hotels[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Hotels
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
