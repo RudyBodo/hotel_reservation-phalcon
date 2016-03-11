88ce0e3609b7b946ac81805b077afa3dbc132d34
rudybodo <transkumba@gmail.com>
2016-03-12 04:06:34 +0800

    modify index controller

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,13 +5,9 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-        $this->view->hotel = Hotels::find();
+
     }
 
-    public function detailAction($hotelsId)
-    {
-        $this->view->hotels = Hotels::findFirst($hotelsId)
-    }
 
 }
 
